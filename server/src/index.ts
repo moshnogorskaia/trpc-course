e50b3f17a2b8b170f3062d20a85a775aa44190cf
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2026-06-22 19:47:20 +0200

    Add express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,22 @@
-const sayHello = (subject: string): void => {
-    console.log('Hello ' + subject);
-  };
-  
-  sayHello('TypeScript');
-  
+import express from 'express';
+import cors from 'cors';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { createContext } from './context';
+import { appRouter } from './router';
+
+const app = express();
+
+app.use(cors());
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+
+app.listen(4000);
+
+export type AppRouter = typeof appRouter;
