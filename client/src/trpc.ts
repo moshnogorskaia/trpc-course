9e189b23afab5f8c5d5e8fe0b03ba105c7847e94
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2026-06-26 11:50:47 +0200

    Add react query adapter to trpc

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -1,10 +1,12 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { createTRPCReact, httpBatchLink } from '@trpc/react-query';
 import type { AppRouter } from '../../server/src/index';
 
-export const trpc = createTRPCProxyClient<AppRouter>({
-    links: [
-      httpBatchLink({
-        url: 'http://localhost:4000/trpc',
-      }),
-    ],
-  });
+export const trpc = createTRPCReact<AppRouter>();
+
+export const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:4000/trpc',
+    }),
+  ],
+});
